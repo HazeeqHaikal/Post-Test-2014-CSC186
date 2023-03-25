b93d87ca6b1287afd8b86eb938a133723bc21d29
HazeeqHaikal <haikalroslan740@gmail.com>
2023-03-25 15:39:08 +0800

    almost finished

--- question1/TransTown.java
+++ question1/TransTown.java
@@ -9,19 +9,55 @@ public class TransTown {
         status = input.nextInt();
 
         int zoneNumber;
-        System.out.println("Enter your zone number (1-2): ");
+        System.out.print("Enter your zone number (1-2): ");
         zoneNumber = input.nextInt();
 
         int paymentMethod;
-        System.out.println("1. Cash\n2. Smart Rider\n\nEnter your payment method (1-2): ");
+        System.out.print("1. Cash\n2. Smart Rider\n\nEnter your payment method (1-2): ");
         paymentMethod = input.nextInt();
 
+        double fare = calculateFare(status, zoneNumber, paymentMethod);
+        System.out.println("Your fare is RM" + fare);
+
+        input.close();
+    }
+
+    static double calculateFare(int status, int zoneNumber, int paymentMethod) {
         double discount = 0;
 
         if (paymentMethod == 2) {
             discount = 0.15;
         }
 
-        
+        double price = 0;
+
+        if (status == 1) {
+            if (zoneNumber == 1) {
+                price = 2.50;
+            } else if (zoneNumber == 2) {
+                price = 3.7;
+            }
+        } else if (status == 2) {
+            if (zoneNumber == 1) {
+                price = 1.00;
+            } else if (zoneNumber == 2) {
+                price = 1.50;
+            }
+        } else if (status == 3) {
+            if (paymentMethod == 2) {
+                price = 0.50;
+            } else {
+                if (zoneNumber == 1) {
+                    price = 2.50;
+                } else if (zoneNumber == 2) {
+                    price = 3.70;
+                }
+
+            }
+        }
+
+        double finalPrice = price - (price * discount);
+
+        return finalPrice;
     }
 }
